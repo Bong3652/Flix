ef90024fca9690bf4d7a2ee91a665e2f5829b7f8
Yong Dong <yongdong11467@gmail.com>
2019-01-08 20:45:04 -0500

    first commit

---
+++ Flix/MovieTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieTableViewCell.swift
+//  Flix
+//
+//  Created by APPLE on 1/8/19.
+//  Copyright © 2019 Bong. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
